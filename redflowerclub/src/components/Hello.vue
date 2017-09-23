dc8c5e4b1f50fb8ad0bce438ad3b190f9535f86d
Pusugar <pusugar.sps@gmail.com>
2017-09-23 11:55:19 +0800

    replace default logo and modified hello.vue simply

--- redflowerclub/src/components/Hello.vue
+++ redflowerclub/src/components/Hello.vue
@@ -1,22 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>{{ enMsg }}</h1>
+    <h1>{{ cnMsg }}</h1>
+    <button class="espect-btn" type="button" name="button">敬请期待</button>
   </div>
 </template>
 
@@ -25,7 +11,8 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      enMsg: 'Welcome to Your Red Flower Club',
+      cnMsg: '欢迎来到红花会'
     }
   }
 }
@@ -49,5 +36,14 @@ li {
 
 a {
   color: #42b983;
+}
+
+.espect-btn {
+  width: 120px;
+  height: 60px;
+  font-size: 18px;
+  background: #fff;
+  color: #e9002d;
+  border: 3px solid #e9002d;
 }
 </style>
